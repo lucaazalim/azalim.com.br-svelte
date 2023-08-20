7aba6c4cdb7f3001e65adba012367d512a0ea055
Luca Azalim <lucaazalim@gmail.com>
2023-08-19 21:16:23 -0300

    Initial commit.

---
+++ src/lib/SocialLink.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  export let name: string;
+  export let icon: string;
+  export let color: string = "#363636";
+  export let url: string;
+</script>
+
+<a href={url} target="_blank">
+  <div class="link-icon" style="background-color: {color}">
+    <i class={icon} />
+  </div>
+  <div class="link-name">
+    {name}
+  </div>
+</a>
+
+<style>
+  a {
+    display: flex;
+    color: white;
+    text-decoration: none;
+    text-align: center;
+    transition: filter 0.3s;
+  }
+
+  a:hover {
+    filter: brightness(120%);
+  }
+
+  .link-icon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50px;
+
+    font-size: 120%;
+
+    padding: 5px;
+    margin-right: 3px;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+  }
+
+  .link-name {
+    flex: 1;
+    padding: 15px;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+    background-color: #363636;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,115 @@
+<script lang="ts">
+  import SocialLink from "$lib/SocialLink.svelte";
+
+  let avatar: string;
+  let name: string;
+  let bio: string;
+
+  fetch("https://api.github.com/users/lucaazalim")
+    .then((response) => response.json())
+    .then((data) => {
+      avatar = data.avatar_url;
+      name = data.name;
+      bio = data.bio;
+    });
+</script>
+
+<main>
+  <header>
+    <img id="avatar" src={avatar} />
+    <h1 id="name">{name}</h1>
+    <p id="bio">{bio}</p>
+  </header>
+  <div id="links">
+    <SocialLink
+      name="GitHub"
+      icon="fa-brands fa-github"
+      color="#333"
+      url="https://github.com/lucaazalim"
+    />
+    <SocialLink
+      name="LinkedIn"
+      icon="fa-brands fa-linkedin"
+      color="#0077B5"
+      url="https://www.linkedin.com/in/luca-azalim-04153999/"
+    />
+    <SocialLink
+      name="Twitter"
+      icon="fa-brands fa-twitter"
+      color="#1DA1F2"
+      url="https://twitter.com/lucaazalim"
+    />
+    <SocialLink
+      name="Threads"
+      icon="fa-solid fa-at"
+      color="#333"
+      url="https://www.threads.net/@lucaazalim"
+    />
+    <SocialLink
+      name="Instagram"
+      icon="fa-brands fa-instagram"
+      color="#E1306C"
+      url="https://www.instagram.com/lucaazalim"
+    />
+    <SocialLink
+      name="Comidas do Luca"
+      icon="fa-solid fa-burger"
+      color="#E1306C"
+      url="https://www.instagram.com/comidasdoluca/"
+    />
+    <SocialLink
+      name="Filmes e séries assistidos"
+      icon="fas fa-film"
+      color="#1da666"
+      url="https://docs.google.com/spreadsheets/d/15niBHmP8eMCLUAH7XefBBk2QyjQj0X57hQSq8tQXaQ0/edit#gid=991418092"
+    />
+    <SocialLink
+      name="Rede Sky"
+      icon="fa-solid fa-heart"
+      url="https://redesky.com/"
+    />
+  </div>
+</main>
+
+<style>
+  :global(body, html) {
+    font-family: "Roboto", sans-serif;
+    background-color: #020202;
+    color: #e7e7e7;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  main {
+    max-width: 500px;
+    margin: 10px;
+  }
+
+  header {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #avatar {
+    width: 200px;
+    border-radius: 50%;
+    margin-bottom: 1rem;
+  }
+
+  #name {
+    margin: 0;
+  }
+
+  #bio {
+    text-align: center;
+  }
+
+  #links {
+    display: grid;
+    gap: 1rem;
+  }
+</style>
